df0acadb42bbf019f8410b55be79d0e3049852fb
Braundt Lau <braundt.lau@gmail.com>
2020-09-15 13:26:16 +0800

    Add some testing code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h4>{{ test }}</h4>
+    <p>This should work</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
